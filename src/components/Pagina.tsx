5e38ebfb471117aaaf29fdc727603e461ad6057b
Anderson Almeida <andersonpa10pa@gmail.com>
2025-05-13 21:25:40 -0300

    Configuração do componente Pagina

---
+++ src/components/Pagina.tsx
@@ -0,0 +1,16 @@
+interface PaginaProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  children?: any
+}
+
+export default function Pagina({children}:PaginaProps) {
+  return (
+    <div className="
+      flex flex-col items-center justify-center min-h-screen
+      bg-gradient-to-r from-zinc-900 to-black
+      text-5xl font-black
+    ">
+      {children}
+    </div>
+  )
+}
