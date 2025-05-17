6e56671b297a7714a2d4551aa744360d25f6c0f9
Anderson Almeida <andersonpa10pa@gmail.com>
2025-05-17 11:24:36 -0300

    Criação de rotas dinâmicas

--- src/pages/rotas/dinamicas/[ano]/[mes]/[dia]/agenda.tsx
+++ src/pages/rotas/dinamicas/[ano]/[mes]/[dia]/agenda.tsx
@@ -1,13 +1,13 @@
 import Pagina from '@/components/Pagina'
 import { useRouter } from 'next/router'
 
-export default function PaginaProdutoPorId() {
+export default function PaginaAgenda() {
   const router = useRouter()
-
+  const { dia, mes, ano } = router.query
   return (
     <Pagina>
       <div>
-        <p>Produto: {router.query.id}</p>
+        {dia}/{mes}/{ano}
       </div>
     </Pagina>
   )
--- src/pages/rotas/dinamicas/curso/[...params].tsx
+++ src/pages/rotas/dinamicas/curso/[...params].tsx
@@ -1,13 +1,15 @@
 import Pagina from '@/components/Pagina'
 import { useRouter } from 'next/router'
 
-export default function PaginaProdutoPorId() {
+export default function PaginaCurso() {
   const router = useRouter()
+  const [curso, aula] = (router.query.params ?? []) as string[]
 
   return (
     <Pagina>
       <div>
-        <p>Produto: {router.query.id}</p>
+        <p>Curso: {curso}</p>
+        <p>Aula: {aula ?? 1}</p>
       </div>
     </Pagina>
   )
